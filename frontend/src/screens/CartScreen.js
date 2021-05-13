dc1907f14ef463141664d1d926c8127d172eb097
billiea96 <billiea48@gmail.com>
2021-05-13 18:20:17 +0700

    Create Redux for Cart, localstorage cartItems

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../actions/cartAction";
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
@@ -6,6 +8,12 @@ export default function CartScreen(props) {
     const params = new URLSearchParams(search);
     // const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
     const qty = params.get('qty');
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatchEvent, productId, qty]);
     return (
         <div>
             <h1>Cart Screen</h1>
