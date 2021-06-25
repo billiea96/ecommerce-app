d6e5fc65c3e71ae85d6cb60aa0f5a7f9daef6bc3
billiea96 <billiea48@gmail.com>
2021-06-25 21:32:36 +0700

    Order details backend, screen, action, reducer

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { CART_EMPTY } from "../constants/cartContants";
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants";
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({
@@ -30,3 +30,29 @@ export const createOrder = (order) => async (dispatch, getState) => {
     });
   }
 };
+
+export const detailsOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({
+    type: ORDER_DETAILS_REQUEST,
+    payload: orderId
+  });
+
+  const { userSignin: { userInfo } } = getState();
+  try {
+    const { data } = await axios.get(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` }
+    });
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data.order
+    });
+  } catch (error) {
+    const message = error.response && error.response.data.message
+      ? error.response.data.message
+      : error.message;
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload: message
+    });
+  }
+}
