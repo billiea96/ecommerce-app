59e539d3f6aa643b2ac92ed05cd17ab9fd25ea05
billiea96 <billiea48@gmail.com>
2021-07-06 21:06:14 +0700

    Order history screen + backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from "./screens/HomeScreen";
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
@@ -35,20 +36,23 @@ function App() {
                                 <span className="badge">{cartItems.length}</span>
                             )}
                         </Link>
-                        {
-                            userInfo ? (
-                                <div className="dropdown">
-                                    <Link to="#">
-                                        {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
-                                    </Link>
-                                    <ul className="dropdown-content">
+                        {userInfo ? (
+                            <div className="dropdown">
+                                <Link to="#">
+                                    {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                                </Link>
+                                <ul className="dropdown-content">
+                                    <li>
+                                        <Link to="/orderhistory">Order History</Link>
+                                    </li>
+                                    <li>
                                         <Link to="/signout" onClick={signoutHandler}>Sign Out</Link>
-                                    </ul>
-                                </div>
-                            ) : (
-                                <Link to="/signin">Sign In</Link>
-                            )
-                        }
+                                    </li>
+                                </ul>
+                            </div>
+                        ) : (
+                            <Link to="/signin">Sign In</Link>
+                        )}
                     </div>
                 </header>
                 <main>
@@ -61,6 +65,7 @@ function App() {
                     <Route path="/payment" component={PaymentMethodScreen}></Route>
                     <Route path="/placeorder" component={PlaceOrderScreen}></Route>
                     <Route path="/order/:id" component={OrderScreen}></Route>
+                    <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
                     <Route path="/" component={HomeScreen} exact></Route>
                 </main>
                 <footer className="row center">
--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { CART_EMPTY } from "../constants/cartContants";
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "../constants/orderConstants";
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_MINE_LIST_FAIL, ORDER_MINE_LIST_REQUEST, ORDER_MINE_LIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({
@@ -65,7 +65,7 @@ export const payOrder = (order, paymentResult) => async (dispatch, getState) => 
 
   const { userSignin: { userInfo } } = getState();
   try {
-    const { data } = axios.put(`/api/orders/${order._id}/pay`, paymentResult, {
+    const { data } = await axios.put(`/api/orders/${order._id}/pay`, paymentResult, {
       headers: { Authorization: `Bearer ${userInfo.token}` }
     });
     dispatch({
@@ -81,4 +81,25 @@ export const payOrder = (order, paymentResult) => async (dispatch, getState) => 
       payload: message
     });
   }
+};
+
+export const listOrderMine = () => async (dispatch, getState) => {
+  dispatch({ type: ORDER_MINE_LIST_REQUEST });
+  const { userSignin: { userInfo }} = getState();
+  try {
+    const { data } = await axios.get(`/api/orders/mine`, {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      }
+    });
+    dispatch({ type: ORDER_MINE_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    const message = error.response && error.response.data.message
+      ? error.response.data.message
+      : error.message;
+    dispatch({
+      type: ORDER_MINE_LIST_FAIL,
+      payload: message
+    });
+  } 
 };
