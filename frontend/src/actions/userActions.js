f57451c69d2013e217c2cfbee7386596c1582e0d
billiea96 <billiea48@gmail.com>
2021-07-10 21:47:52 +0700

    Profile screen & backend

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userContants";
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userContants";
 
 export const register = (name, email, password) => async (dispatch) => {
   dispatch({
@@ -55,3 +55,26 @@ export const signout = () => (dispatch) => {
     type: USER_SIGNOUT
   });
 }
+
+export const detailsUser = (userId) => async (dispatch, getState) => {
+  dispatch({
+    type: USER_DETAILS_REQUEST
+  });
+
+  const { userSignin: { userInfo } } = getState();
+  try {
+    const { data } = await axios.get(`/api/users/${ userId }`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data
+    });
+  } catch (error) {
+    const message = error.response && error.response.data.message ? error.response.data.message : error.message;
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload: message
+    });
+  }
+};
